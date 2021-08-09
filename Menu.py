0d2c0f090ff7e663f889ac3354246ebeb40b5640
jordylh2001 <56316765+jordylh2001@users.noreply.github.com>
2021-08-09 06:28:04 -0500

    Actualizacion

--- Menu.py
+++ Menu.py
@@ -1,30 +1,27 @@
 import tkinter as tk
 from tkinter import ttk, CENTER
 
-import InterfazNewton
 
 
-def menu():
-    InterfazNewton.pack_forget()
-    principal.pack(side="top", fill="both", expand=True)
+class Menu:
+    def __init__(self, master):
+        root.title("Calculadora")
+        root.geometry("400x400")
+        principal = tk.Frame(root)
+        button = tk.Button(principal, text="Newton", height=5, width=30, command=newton)
+        button2 = tk.Button(principal, text="Interpolacion", height=5, width=30, command=Interpolacion)
+        button.place(relx=0.5, rely=.25, anchor=CENTER)
+        button2.place(relx=0.5, rely=.5, anchor=CENTER)
 
+    def newton(self):
+        obj = InterfazNewton.InterfazNewton()
+        pass
 
-def newton():
-    principal.pack_forget()
-    InterfazNewton.pack(side="top", fill="both", expand=True)
-
-
-def Interpolacion():
-    pass
+    def Interpolacion(self):
+        pass
 
 
 root = tk.Tk()
 root.eval('tk::PlaceWindow . center')
-root.title("Calculadora")
-root.geometry("400x400")
-principal = tk.Frame(root)
-button = tk.Button(principal, text="Newton", height=5, width=30, command=newton())
-button2 = tk.Button(principal, text="Interpolacion", height=5, width=30, command=Interpolacion())
-button.place(relx=0.5, rely=.25, anchor=CENTER)
-button2.place(relx=0.5, rely=.5, anchor=CENTER)
+app = Menu(root)
 root.mainloop()
