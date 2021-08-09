ac652238b95de488f44977ae14d77cda139d241d
jordylh2001 <56316765+jordylh2001@users.noreply.github.com>
2021-08-09 05:53:37 -0500

    Actualizacion

--- InterfazInterpolacion.py
+++ InterfazInterpolacion.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 from tkinter import ttk, CENTER
 
+import interpolation
+
 
 class InterfazInterpolacion:
+    #coordenadas
+    x = list()
+    y = list()
+    #indice
+    z = 0
 
     def __init__(self, master):
+        self.message = tk.StringVar(value="Resultado    :")
         self.text = tk.StringVar()
         self.text2 = tk.StringVar()
         self.text3 = tk.StringVar()
@@ -44,33 +52,32 @@ class InterfazInterpolacion:
         self.tree.column("# 2", anchor=CENTER)
         self.tree.heading("# 2", text="y")
         self.tree.place(x=50, y=200)
+        #Resultado
+        self.label4 = tk.Label(master, text=self.message)
+        self.label4.place(x=50, y=350)
         # self.place(width=1100, height=400)
 
     def calcular(self):
-        print("Calculate")
-        data = self.text.get()
-        data2 = self.text2.get()
+        # print("Calculate")
         data3 = self.text3.get()
-        print(data, data2, data3)
-        print(self.text.get(), self.text2.get(), self.text3.get())
+        obj = interpolation.Interpolation(self.x, self.y)
+        obj.compute2(data3)
         return None
 
     def agregar(self):
-        print("ADD")
+        # print("ADD")
         data = self.text.get()
         data2 = self.text2.get()
-        data3 = self.text3.get()
-        print(data, data2, data3)
-        print(self.text.get(), self.text2.get(), self.text3.get())
+        self.x.append(data)
+        self.y.append(data2)
+        self.z = self.z + 1
+        self.tree.insert(parent='', index=self.z, iid=self.z, text='', values=(data, data2))
+        # print(data, data2, data3)
+        # print(self.text.get(), self.text2.get(), self.text3.get())
         return None
 
     def regresar(self):
-        print("RETURN")
-        data = self.text.get()
-        data2 = self.text2.get()
-        data3 = self.text3.get()
-        print(data, data2, data3)
-        print(self.text.get(), self.text2.get(), self.text3.get())
+        # print("RETURN")
         return None
 
 
--- InterfazNewton.py
+++ InterfazNewton.py
@@ -5,6 +5,10 @@ from tkinter import ttk, CENTER
 class InterfazNewton:
 
     def __init__(self, master):
+        self.text = tk.StringVar()
+        self.text2 = tk.StringVar()
+        self.text3 = tk.StringVar()
+        self.text4 = tk.StringVar()
         self.master = master
         master.title("Newton")
         master.geometry("1100x400")
@@ -12,25 +16,26 @@ class InterfazNewton:
         # Expresion.
         self.label = tk.Label(master, text="introduzca la expresion")
         self.label.place(x=50, y=20)
-        self.entry = ttk.Entry(root)
+        self.entry = ttk.Entry(master, textvariable=self.text)
         self.entry.place(x=50, y=50)
         #Datos de X0
         self.label2 = tk.Label(root, text="El valor de x(0)")
         self.label2.place(x=200, y=20)
-        self.entry2 = ttk.Entry(master)
+        self.entry2 = ttk.Entry(master, textvariable=self.text2)
         self.entry2.place(x=200, y=50)
         #Datos de la cantidad de resultados
         self.label3 = tk.Label(master, text="cantidad de veces")
         self.label3.place(x=350, y=20)
-        self.entry3 = ttk.Entry(master)
+        self.entry3 = ttk.Entry(master, textvariable=self.text3)
         self.entry3.place(x=350, y=50)
         #Datos del numero maximo
         self.label4 = tk.Label(master, text="hasta que numero")
         self.label4.place(x=500, y=20)
-        self.entry4 = ttk.Entry(master)
+        self.entry4 = ttk.Entry(master, textvariable=self.text4)
         self.entry4.place(x=500, y=50)
-        # Posicionarla en la ventana.
-        self.entry3.place(x=350, y=50)
+        #Botones
+        self.button = tk.Button(root, text="Regresar", command=self.Regresar)
+        self.button.place(x=50, y=80)
         self.button = tk.Button(master, text="Calcular", command=self.calcular)
         self.button.place(x=350, y=150)
         # Create an object of Style widget
@@ -53,17 +58,17 @@ class InterfazNewton:
         # self.place(width=1100, height=400)
 
     def calcular(self):
-        selected = int(self.selected.get())
-        if selected (int) == 1:
-            print("1")
-            '''callsNewton'''
-        elif selected (int) == 2:
-            '''Calls Interpolated'''
-            print("2")
-        pass
+        #print("Calculate")
+        data = self.text.get()
+        data2 = self.text2.get()
+        data3 = self.text3.get()
+        data4 = self.text4.get()
+        #print(data, data2, data3)
+        #print(self.text.get(), self.text2.get(), self.text3.get())
+        return None
 
     def regresar(self):
-        pass
+        return None
 
 
 root = tk.Tk()
