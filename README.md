6591c89c06265237e94ca60d72ce9d0dcb2f60ac
Crxwler <loco7139@gmail.com>
2021-08-09 01:59:29 -0500

    Setup config

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Solution of equations by numerical methods
   * Newton
   * Interpolation
+## Build use
+  You need pyexe to build the project
+  ```python
+    pip install py2exe
+```
+  After you have pyexe, just needs to use:
+  ```python:
+    py setup.py pyexe
+   ```
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    name = "numerical_methods",
+    version= "1.0",
+    description="Numerical Methods",
+    author="Juan Carlos Monreal Romero & Jordy Lagunas Higuera",
+    author_email="jcmonrealr@gmail.com",
+    url="https://github.com/Crxwler/Numerical-Methods",
+    license="GNU General Public License v2.0",
+    scripts=["newton.py"],
+    console=["newton.py"],
+    options={"py2exe": {"bundle_files": 1}},
+    zipfile=None,
+)
